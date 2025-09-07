3dcab0b6e346b5a9821658b6e22fd703851c454d
Shivam24S <shivamshrivastav8055@gmail.com>
2025-09-07 11:44:07 +0530

    effect in jquery

---
+++ jquery/effect/script.js
@@ -0,0 +1,57 @@
+// hide effect
+
+$("#hide").click(() => {
+  $("#img").hide();
+});
+
+// show effect
+
+$("#show").click(() => {
+  $("#img").show();
+});
+
+// toggle effect
+
+$("#toggle").click(() => {
+  $("#img").toggle(2000);
+});
+
+// fades effect
+
+$("#fadeIn").click(() => {
+  $("#img").fadeIn(1000);
+});
+
+// show effect
+
+$("#fadeOut").click(() => {
+  $("#img").fadeOut(1000);
+});
+
+// toggle effect
+
+$("#fadeToggle").click(() => {
+  $("#img").fadeToggle(1000);
+});
+
+// slides effect
+
+$("#slideUp").click(() => {
+  $("#img").slideUp(1000);
+});
+
+// show effect
+
+$("#slideDown").click(() => {
+  $("#img").slideDown(1000);
+});
+
+// toggle effect
+
+$("#slideToggle").click(() => {
+  $("#img").slideToggle(1000);
+});
+
+$("#stop").click(() => {
+  $("#img").stop();
+});
