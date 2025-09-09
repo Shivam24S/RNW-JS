4057691d761b460196c1a0c7cf6fa1a90e0e91e2
Shivam24S <shivamshrivastav8055@gmail.com>
2025-09-09 05:37:10 +0530

    jquery selectors

--- jquery/selectors/script.js
+++ jquery/selectors/script.js
@@ -1,3 +1,22 @@
-function func1() {
-  $("#img1").fadeToggle();
-}
+// function func1() {
+//   $("#img1").fadeToggle();
+// }
+
+// $("*").css("background-color", "black");
+
+// $("*").css({
+//   "background-color": "black",
+//   color: "white",
+// });
+
+// $(selector).action()
+
+$("div>p").css("color", "red");
+
+$("ul>li").css("color", "blue");
+
+$(".class").hide();
+
+$("#id").css("font-size", "30px");
+
+$("[href]").css("color", "orange");
