381f51c7909b494a1d68adcb94e5aa91efed66a1
Shivam24S <shivamshrivastav8055@gmail.com>
2025-10-02 10:06:01 +0530

    datatype

--- ch-1/script.js
+++ ch-1/script.js
@@ -1,3 +1,5 @@
 document.getElementById("btn").addEventListener("click", function () {
   document.getElementById("status").textContent = "logged in";
 });
+
+console.log("hello");
---
+++ ch-3/datatypes.js
@@ -0,0 +1,41 @@
+// // data by value
+
+// const num1 = 10;
+
+// let num2 = num1;
+
+// console.log("num1", num1);
+
+// console.log("num2", num2);
+
+// num2 = 20;
+
+// console.log("num1", num1);
+
+// console.log("num2", num2);
+
+//
+
+const person = {
+  name: "alice",
+};
+
+const person2 = person;
+
+// console.log("Person 1", person);
+
+// console.log("person 2", person2);
+
+// person2.name = "john";
+
+// console.log("person 1", person);
+
+// console.log("person 2", person2);
+
+console.log("type of person 2", typeof person2);
+
+console.log("type of null", typeof null);
+
+const str = "hello";
+
+console.log(typeof str);
