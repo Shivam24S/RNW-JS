b16fb6082be0f7414b6adb6049f6d6409024d2d2
Shivam24S <shivamshrivastav8055@gmail.com>
2025-10-03 10:08:40 +0530

    operators

---
+++ ch-4/operator.js
@@ -0,0 +1,51 @@
+// arithmetic
+
+// let a = 10;
+
+// let b = 5;
+
+// console.log("addition", a + b);
+
+// console.log("subtraction", a - b);
+
+// console.log("mul", a * b);
+
+// console.log("division", a / b);
+
+// console.log("modulo", a % b);
+
+// console.log("exponential", a ** b);
+
+// assignment
+
+// let x = 2;
+
+// x += 3;
+
+// console.log("x 1", x);
+
+// x = x + 3;
+
+// console.log("x 2", x);
+
+// comparison
+
+// let a = 5;
+
+// let b = "5";
+
+// console.log(a === b);
+
+// let d = 65;
+
+// let e = "A";
+
+// console.log(d == e);
+
+let a = 10;
+
+let b = 10;
+
+console.log(a <= b);
+
+// console.log(a > b);
