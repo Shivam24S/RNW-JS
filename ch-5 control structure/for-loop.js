9ddba6d815729251bfda09dd091991c4d1d5468a
Shivam24S <shivamshrivastav8055@gmail.com>
2025-10-07 10:07:19 +0530

    loops code added

---
+++ ch-5 control structure/for-loop.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 1000; i++) {
+  console.log(i);
+}
+
+//
+
+const subject = ["maths", "physics", "english", "chemistry", "science"];
+
+for (let i = 0; i < subject.length; i++) {
+  console.log(subject[i]);
+}
