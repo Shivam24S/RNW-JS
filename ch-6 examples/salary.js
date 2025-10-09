c1e1fa5ec74ce8915c692303b2299d3e74262774
Shivam24S <shivamshrivastav8055@gmail.com>
2025-10-09 10:04:15 +0530

    salary code added

---
+++ ch-6 examples/salary.js
@@ -0,0 +1,17 @@
+document.getElementById("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const basicSalary = document.getElementById("basicSalary").value;
+
+  const HRA = parseFloat(document.getElementById("hra").value);
+
+  const DA = parseFloat(document.getElementById("da").value);
+
+  const TA = parseFloat(document.getElementById("ta").value);
+
+  //   const hraAmount = (basicSalary*hra)/100
+
+  const grossSalary = basicSalary * (1 + (HRA + DA + TA) / 100);
+
+  document.getElementById("grossSalary").innerHTML = `<h1>₹${grossSalary}</h1>`;
+});
