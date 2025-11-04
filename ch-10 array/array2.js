658a4f1c11b5ba9ecf729c82d38fabc6481a67b4
Shivam24S <shivamshrivastav8055@gmail.com>
2025-11-04 10:06:27 +0530

    array methods added

--- ch-10 array/array2.js
+++ ch-10 array/array2.js
@@ -53,3 +53,70 @@ ageResult = ageValues.some((age) => {
 });
 
 console.log("age result", ageResult);
+
+// reverse method
+
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log("reverse number", number.reverse());
+
+// indexOf
+
+let vegetables = ["potato", "tomato", "ginger", "chilli", "cucumber"];
+
+console.log("index of result", vegetables.indexOf("ginger"));
+
+// if not available
+console.log("index of result", vegetables.indexOf("brinjal"));
+
+// includes  methods
+
+console.log(
+  "includes method if element available",
+  vegetables.includes("tomato")
+);
+
+// if element is not available
+
+console.log(vegetables.includes("Cabbage"));
+
+//
+
+let alphabet = ["a", "d", "b", "e", "c", "s", "z"];
+
+console.log("sorting alphabet", alphabet.sort());
+
+// sorting number
+
+number = [7, 8, 1, 5, 9, 6, 3, 4, 5, 6, 2];
+
+console.log("sorting number", number.sort());
+
+console.log(
+  "sorting number in descending order",
+  number.sort((a, b) => b - a)
+);
+
+//
+
+const persons = [
+  { name: "utsav", age: 19 },
+  { name: "dharmik", age: 19 },
+  { name: "vaishali", age: 18 },
+  {
+    name: "brij",
+    age: 19,
+  },
+  {
+    name: "partik",
+    age: 19,
+  },
+];
+
+const checkAge = persons.find((person) => person.age > 18);
+
+console.log("result", checkAge);
+
+const checkAgeResult = persons.findIndex((person) => person.age > 18);
+
+console.log("findIndexOf result", checkAgeResult);
