e82718345e8fd7ad14560db8b44a2016b93e1a80
Shivam24S <shivamshrivastav8055@gmail.com>
2025-11-07 10:05:54 +0530

    object method added

---
+++ ch-11 object/1.object.js
@@ -0,0 +1,87 @@
+// 1. object literals
+
+let studentDetails = {
+  name: "alice",
+  age: 10,
+};
+
+// constructor
+
+const MeasurementDetails = new Object();
+
+MeasurementDetails.length = 200;
+
+MeasurementDetails.width = 300;
+
+MeasurementDetails.height = 400;
+
+console.log(MeasurementDetails);
+
+// accessing object property
+
+const personDetail = {
+  name: "alice",
+  age: 20,
+  profession: "worker",
+  city: "mumbai",
+};
+
+console.log(personDetail);
+
+console.log(personDetail.name);
+
+// using bracket
+
+console.log(personDetail["profession"]);
+
+personDetail.profession = "banker";
+
+personDetail.department = "finance";
+
+console.log(personDetail);
+
+// destructing
+
+const { profession } = personDetail;
+
+console.log(profession);
+
+const { name: names } = personDetail;
+
+console.log(names);
+
+const student1 = {
+  id: 9688,
+  name: "brij",
+  age: 19,
+  course: "fullstack development",
+};
+
+const student2 = {
+  id: 9035,
+  name: "vaishali",
+  age: 18,
+  course: "fullstack development",
+};
+
+// now defining object using function constructor
+
+function StudentDetail(id, name, age, course) {
+  (this.id = id), (this.name = name), (this.age = age), (this.course = course);
+}
+
+const student3 = new StudentDetail(
+  9772,
+  "Dharmik",
+  19,
+  "fullstack development"
+);
+
+const student4 = new StudentDetail(8557, "pratik", 19, "fullstack development");
+
+StudentDetail.prototype.email = "abcd@gmail.com";
+
+console.log("student email", student3.email);
+
+console.log(student3);
+console.log(student4);
