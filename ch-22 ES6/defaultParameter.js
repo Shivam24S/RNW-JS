9c7a6ed07e3dbb6ef81a74070e967ea32cce6e3b
shivam24s <shivamshrivastav8055@gmail.com>
2025-12-09 10:04:24 +0530

    symbol and default parameter

---
+++ ch-22 ES6/defaultParameter.js
@@ -0,0 +1,34 @@
+
+
+
+const game = (game) =>{
+
+    console.log(game)
+}
+
+game("gta-5")
+
+
+// 
+
+
+const game2 = (game) =>{
+    console.log("i am playing",game)
+}
+
+game2("chess")
+
+
+
+const game3 = (game="horizon-5") =>{
+    console.log("i am playing",game)
+}
+
+game3()
+
+
+const game4 = (game="horizon-5") =>{
+    console.log("i am playing",game)
+}
+
+game3("god of war")
